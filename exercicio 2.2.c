91b55ffc991b1cd5125bbdee2f42f67a0f2d0e78
Marina Rangel <marina.anapurus@hotmail.com>
2018-10-08 09:21:29 -0300

    Update exercicio 2.2.c

--- exercicio 2.2.c
+++ exercicio 2.2.c
@@ -10,7 +10,13 @@ int main()
    printf("Digite um numero para n2:");
    scanf("%d",&n2);
    
-   for(i=n1+1;i<n2;i++)
-     printf("%d ",i);
+   if(n1>n2){
+    for(i=n1+1;i<n2;i++)
+      printf("%d ",i);
+   }
+   else{
+      for(i=n2+1;i<n1;i++)
+         printf("%d ",i);
+   }
    return 0;
 }
