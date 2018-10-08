f9c8ded422c2545b9295a3c955fbd7c5a8f6882f
Marina Rangel <marina.anapurus@hotmail.com>
2018-10-08 09:22:09 -0300

    Update exercicio 2.2.c

--- exercicio 2.2.c
+++ exercicio 2.2.c
@@ -11,8 +11,8 @@ int main()
    scanf("%d",&n2);
    
    if(n1>n2){
-    for(i=n1+1;i<n2;i++)
-      printf("%d ",i);
+     for(i=n1+1;i<n2;i++)
+        printf("%d ",i);
    }
    else{
       for(i=n2+1;i<n1;i++)
