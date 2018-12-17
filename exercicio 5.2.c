2eb68d88a9f2b5702fcb8735900d7f7e6b73e0dd
Marina Rangel <marina.anapurus@hotmail.com>
2018-12-16 23:21:54 -0200

    Update exercicio 5.2.c

--- exercicio 5.2.c
+++ exercicio 5.2.c
@@ -14,11 +14,14 @@ int main()
 	printf("Digite seu saldo: ");
 	scanf("%d",&minha_conta2);
 	
-	printf("%d %d\n",minha_conta1,minha_conta2);
-	if(minha_conta1>minha_conta2)
+	printf("Conta 1  Conta 2\n");
+	printf("******   *******\n");
+	printf("%d      %d\n",minha_conta1,minha_conta2);
+	if(minha_conta1>minha_conta2){
 	  compra(&minha_conta1,500);
+    }
 	else
 	  compra(&minha_conta2,500);
-	printf("%d %d\n",minha_conta1,minha_conta2);
+	printf("%d      %d\n",minha_conta1,minha_conta2);
 	return 0;
 }
