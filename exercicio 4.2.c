c22ba8f15f461be34eb410f8e92bd2679b577e23
Marina Rangel <marina.anapurus@hotmail.com>
2018-12-16 22:40:04 -0200

    Update exercicio 4.2.c

--- exercicio 4.2.c
+++ exercicio 4.2.c
@@ -1,22 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int eh_primo(int n){
+	int i,cont=0;
+	
+	for(i=1;i<=n;i++)
+	{
+		if(n%i==0)
+		  cont++;
+	}
+	if(cont==2)
+	  return 1;
+	else
+	  return 0;
+}
+
 void todos_os_primos(int max)
 {
-	int i,cont,n=2;
+	int i,resp,n=1;
 	
 	while(n<max)
 	{
-	  cont=0;	
-	  for(i=2;i<max;i++)
-	  {
-		if(n%i==0)
-		  cont++;
-	  }  
-	  if(cont==2)
-	    printf("%d\n",n);
+	  resp=eh_primo(n);
+	  if(resp==1)
+	    printf("%d ",n);
 	  n++;
-    }
+	}
 }
 
 int main()
